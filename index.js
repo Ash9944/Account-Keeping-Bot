6f4123b0486fe9509f45f12eae4f4528ccf241e8
AshwajitP-PSGST <aswajith.p@psgsoftwaretechnologies.com>
2024-01-07 00:38:57 +0530

    incuded applogger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const userServices = require('./services/user')
 const config = require('./config.json');
 var mongoUtil = require('./daos/MongodDbUtil');
+var appLogger = require('./logging/appLogger')
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(config.tokenId, { polling: true });
@@ -17,8 +18,10 @@ bot.onText(/start/, async function onEchoText(msg) {
 
     await userServices.signup(msg);
     bot.sendMessage(msg.chat.id, "Welcome to the TG Account Bot Your Account has been created");
+    appLogger.info("User Has been Signed in", JSON.stringify(msg))
   } catch (error) {
     bot.sendMessage(msg.chat.id, error.message)
+    appLogger.error("User  Sign in error", JSON.stringify(error))
   }
 });
 
@@ -29,9 +32,10 @@ bot.onText(/update/, async function onEchoText(msg) {
     }
 
     await userServices.update(msg, bot);
-
+    appLogger.info("Sucessfully updated user", JSON.stringify(msg))
   } catch (error) {
     bot.sendMessage(msg.chat.id, error.message)
+    appLogger.error("update error", JSON.stringify(error))
   }
 });
 
@@ -43,8 +47,10 @@ bot.onText(/fetchdebit/, async function onEchoText(msg) {
 
     var response = await userServices.fetchUserDebit(msg.chat.id);
     bot.sendMessage(msg.chat.id, response);
+    appLogger.info("Sucessfully fetched debit summary", JSON.stringify(msg))
   } catch (error) {
     bot.sendMessage(msg.chat.id, error.message)
+    appLogger.error("fetch debit summary error", JSON.stringify(error))
   }
 });
 
@@ -56,8 +62,10 @@ bot.onText(/fetchcredit/, async function onEchoText(msg) {
 
     var response = await userServices.fetchUserCredit(msg.chat.id);
     bot.sendMessage(msg.chat.id, response);
+    appLogger.info("Sucessfully fetched credit summary", JSON.stringify(msg))
   } catch (error) {
     bot.sendMessage(msg.chat.id, error.message)
+    appLogger.error("fetch credit summary error", JSON.stringify(error))
   }
 });
 
@@ -67,8 +75,10 @@ bot.onText(/creditdetails/, async function onEchoText(msg) {
       throw new Error('Error')
     }
     await userServices.fetchUserCreditTransactions(msg,bot);
+    appLogger.info("Sucessfully fetched credit detailed version", JSON.stringify(msg))
   } catch (error) {
     bot.sendMessage(msg.chat.id, error.message)
+    appLogger.error("fetch credit detailed version error", JSON.stringify(error))
   }
 });
 
@@ -78,8 +88,9 @@ bot.onText(/debitdetails/, async function onEchoText(msg) {
       throw new Error('Error')
     }
     await userServices.fetchUserDebitTransactions(msg, bot);
+    appLogger.info("Sucessfully fetched debit detailed version", JSON.stringify(msg))
   } catch (error) {
     bot.sendMessage(msg.chat.id, error.message)
+    appLogger.error("fetch debit detailed version error", JSON.stringify(error))
   }
 });
-
